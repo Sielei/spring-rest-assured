d52d8d3defbc7f85cab17d0f99b82a209571358b
Sielei Herman <hsielei@gmail.com>
2024-03-23 16:44:36 +0300

    chore: project setup

---
+++ src/main/java/com/spra/springrestassured/SpringRestAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.spra.springrestassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestAssuredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestAssuredApplication.class, args);
+    }
+
+}
