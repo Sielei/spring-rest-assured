a6cbcd0ad3950a6ea0ed5810ad6666387d758609
Sielei Herman <hsielei@gmail.com>
2024-03-24 16:54:31 +0300

    chore: Document the example data for each API json field

--- src/main/java/com/spra/springrestassured/job/core/JobRequest.java
+++ src/main/java/com/spra/springrestassured/job/core/JobRequest.java
@@ -1,15 +1,16 @@
 package com.spra.springrestassured.job.core;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 import java.util.Date;
 
-public record JobRequest(@NotBlank(message = "Job title is required") String jobTitle,
-                         @NotBlank(message = "Description is required") String description,
+public record JobRequest(@Schema(type = "string", example = "Senior Backend Engineer (Java)") @NotBlank(message = "Job title is required") String jobTitle,
+                         @Schema(type = "string", example = "Brief description of the Job.") @NotBlank(message = "Description is required") String description,
                          @NotNull(message = "Job type is required") JobType jobType,
                          Date datePosted,
-                         @Pattern(regexp = "/[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)/",
+                         @Schema(type = "string", example = "https://mycompany.breezy.hr/p/22b17727d5f4-senior-backend-engineer-java") @Pattern(regexp = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)",
                          message = "Invalid Job link") String jobLink) {
 }
--- src/main/java/com/spra/springrestassured/job/core/JobResponse.java
+++ src/main/java/com/spra/springrestassured/job/core/JobResponse.java
@@ -1,7 +1,13 @@
 package com.spra.springrestassured.job.core;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.Date;
 
-public record JobResponse(Long id, String jobTitle, String description, JobType jobType,
-                          Date datePosted, String jobLink) {
+public record JobResponse(@Schema(type = "long", example = "10001") Long id,
+                          @Schema(type = "string", example = "Senior Backend Engineer (Java)") String jobTitle,
+                          @Schema(type = "string", example = "Brief description of the Job.") String description,
+                          JobType jobType,
+                          Date datePosted,
+                          @Schema(type = "string", example = "https://mycompany.breezy.hr/p/22b17727d5f4-senior-backend-engineer-java") String jobLink) {
 }
