7505074edab88711b5e339c2f700c2b3e02dfdb8
Sielei Herman <hsielei@gmail.com>
2024-03-24 17:48:11 +0300

    Document the operation for each endpoint

--- src/main/java/com/spra/springrestassured/job/JobsController.java
+++ src/main/java/com/spra/springrestassured/job/JobsController.java
@@ -4,6 +4,7 @@ import com.spra.springrestassured.job.core.JobRequest;
 import com.spra.springrestassured.job.core.JobResponse;
 import com.spra.springrestassured.job.core.JobService;
 import common.core.PagedContent;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ public class JobsController {
         this.jobService = jobService;
     }
 
+    @Operation(summary = "Add a new Job post")
     @PostMapping
     ResponseEntity<JobResponse> addNewJob(@RequestBody JobRequest jobRequest){
         var newJob = jobService.addNewJob(jobRequest);
@@ -28,23 +30,27 @@ public class JobsController {
         return ResponseEntity.created(location).body(newJob);
     }
 
+    @Operation(summary = "Find all available Job posts")
     @GetMapping
     PagedContent<JobResponse> findAllJobs(@RequestParam(name = "page", defaultValue = "1") Integer pageNo,
                                           @RequestParam(name = "size", defaultValue = "10") Integer pageSize){
         return jobService.findAllJobs(pageNo, pageSize);
     }
 
+    @Operation(summary = "Find Job post by id")
     @GetMapping("/{jobId}")
     JobResponse findById(@PathVariable("jobId") Long jobId){
         return jobService.findJobById(jobId);
     }
 
+    @Operation(summary = "Update a Job Post")
     @PutMapping("/{jobId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void updateJob(@PathVariable("jobId") Long jobId, @RequestBody JobRequest updateJobRequest){
         jobService.updateJob(jobId, updateJobRequest);
     }
 
+    @Operation(summary = "Delete a Job post")
     @DeleteMapping("/{jobId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteJob(@PathVariable("jobId") Long jobId){
